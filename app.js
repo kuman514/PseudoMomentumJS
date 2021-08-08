5d8dd0d7b0ebaf1a0cb50de35503847064a11918
kumankoishi.py <7iggif@naver.com>
2021-08-08 16:34:38 +0900

    Establishing basic login page

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert('hello');
+const loginInput = document.querySelector('#login-form input');
+const loginButton = document.querySelector('#login-form button');
+
+function onLoginClick() {
+  if (loginInput.value === '') {
+
+  } else {
+
+  }
+}
+
+loginButton.addEventListener('click', onLoginClick);
