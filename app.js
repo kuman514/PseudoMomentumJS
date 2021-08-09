4ed43b7b36a80035bc29b85dd8df179315dba772
kumankoishi.py <7iggif@naver.com>
2021-08-09 20:46:44 +0900

    Practicing localStorage

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const link = document.querySelector('#link');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  const username = loginInput.value;
-  loginForm.classList.add('hidden');
-  console.log(`Hello, ${username}!`);
+  if (loginInput.value.length > 0) {
+    const userName = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, userName);
+    paintGreetings(userName);
+  }
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
-link.addEventListener('click', onLinkClick);
+function paintGreetings(userName) {
+  greeting.innerText = `Hello, ${userName}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings(savedUserName);
+}
