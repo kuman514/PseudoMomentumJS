2181eb7979ab6d404cd71d9ca21cea92ce0e257d
kumankoishi.py <7iggif@naver.com>
2021-08-10 16:08:58 +0900

    Implementing appending todos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,22 @@ const todoForm = document.querySelector('#todo-form');
 const todoInput = document.querySelector('#todo-form input');
 const todoList = document.querySelector('#todo-list');
 
+function paintTodo(newTodo) {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  span.innerText = newTodo;
+  const btn = document.createElement('button');
+  li.appendChild(span);
+  li.appendChild(btn);
+  todoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   console.log(todoInput.value);
   const newTodo = todoInput.value;
   todoInput.value = '';
+  paintTodo(newTodo);
 }
 
 todoForm.addEventListener('submit', handleToDoSubmit);
