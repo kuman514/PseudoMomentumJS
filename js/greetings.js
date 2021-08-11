4411f9211ae4556ad98f21c92e5467aa5be5f9e8
kumankoishi.py <7iggif@naver.com>
2021-08-11 17:43:25 +0900

    Added after-login structure

--- js/greetings.js
+++ js/greetings.js
@@ -1,10 +1,15 @@
 const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const link = document.querySelector('#link');
+const afterLogin = document.querySelector('#after-login');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
 const USERNAME_KEY = 'username';
+
+function revealAfterLogin() {
+  afterLogin.classList.remove(HIDDEN_CLASSNAME);
+}
 
 function onLoginSubmit(event) {
   event.preventDefault();
@@ -13,6 +18,7 @@ function onLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     localStorage.setItem(USERNAME_KEY, userName);
     paintGreetings(userName);
+    revealAfterLogin();
   }
 }
 
@@ -28,4 +34,5 @@ if (savedUserName === null) {
 } else {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   paintGreetings(savedUserName);
+  revealAfterLogin();
 }
