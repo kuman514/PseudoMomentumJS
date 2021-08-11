492b960e9401de29a592e0a3683306a4d4133830
kumankoishi.py <7iggif@naver.com>
2021-08-11 17:42:27 +0900

    Removed unnecessary console.log

--- js/background.js
+++ js/background.js
@@ -2,4 +2,3 @@ const images = ['0.jpeg', '1.jpeg', '2.jpeg'];
 const chosenImage = `img/${images[Math.ceil(Math.random() * images.length) - 1]}`;
 
 document.body.style.setProperty('background-image', `url('${chosenImage}')`);
-console.log(document.body.style.backgroundImage);
